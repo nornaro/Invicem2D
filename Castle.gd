82861b831dfd13b4fd97a21c4e3cf32a368d036a
sugo Fenyő István <fuyuhimematsuri@gmail.com>
2023-09-20 20:53:11 +0200

    Partially fixed placement, changed some arrays to sets (dictionary with null values)

---
+++ Castle.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var Castle = preload("res://buildings/Castle.tscn")
+# Called when the node enters the scene tree for the first time.
+
+func _ready():
+	var instance = Castle.instantiate()
+	add_child(instance)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
