80ef1480a8bf9c00999ba1d756d60ef5dd2aade9
sugo <fuyuhimematsuri@gmail.com>
2025-04-20 23:44:44 +0200

    working on netfox

--- Scenes/Multiplayer/Network/NetFox/host.gd
+++ Scenes/Multiplayer/Network/NetFox/host.gd
@@ -9,53 +9,36 @@ var game_id: String = ""
 var local_port:int = Noray.local_port
 
 func host() -> int:
-	# Connect to noray
 	var err = OK
 	var address:String = host_ip
 	err = await Noray.connect_to_host(address)
-	
 	if err != OK:
-		print("Failed to connect to Noray: %s" % error_string(err))
+		push_error("Failed to connect to Noray: %s" % error_string(err))
 		return err
-	
-	# Get IDs
 	Noray.register_host()
-	await Noray.on_pid
-	
-	# Register remote address
-	err = await Noray.register_remote()
+	await Noray.on_pid # Get IDs
+	err = await Noray.register_remote() # Register remote address
 	if err != OK:
-		print("Failed to register remote address: %s" % error_string(err))
+		print_debug("Failed to register remote address: %s" % error_string(err))
 		return err
-	
-	# Our local port is a remote port to Noray, hence the weird naming
-	print("Registered local port: %d" % Noray.local_port)
+	print_debug("Registered local port: %d" % Noray.local_port) # Our local port is a remote port to Noray, hence the weird naming
 	if Noray.local_port <= 0:
 		return ERR_UNCONFIGURED
-
-	print("Starting host on port %s" % port)
-	
+	print_debug("Starting host on port %s" % port)
 	var peer:ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 	err = peer.create_server(port)
 	if err != OK:
 		print("Failed to listen on port %s: %s" % [port, error_string(err)])
 		return err
-
 	get_tree().get_multiplayer().multiplayer_peer = peer
 	print("Listening on port %s" % port)
-	
-	# Wait for server to start
-	while peer.get_connection_status() == MultiplayerPeer.CONNECTION_CONNECTING:
+	while peer.get_connection_status() == MultiplayerPeer.CONNECTION_CONNECTING: # Wait for server to start
 		await get_tree().process_frame
-	
 	if peer.get_connection_status() != MultiplayerPeer.CONNECTION_CONNECTED:
 		OS.alert("Failed to start server!")
 		return FAILED
-	
 	get_tree().get_multiplayer().server_relay = true
-
 	#NetworkTime.start()
-
 	return 0
 
 func _ready() -> void:
